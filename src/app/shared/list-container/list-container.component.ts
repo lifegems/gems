c30d0b335dea325c5f9750f2873cd6b999d0d86b
Michalik, Joshua <joshuasmichalik@gmail.com>
2016-10-15 23:05:25 -0400

    Add search to insort

--- src/app/shared/list-container/list-container.component.ts
+++ src/app/shared/list-container/list-container.component.ts
@@ -1,17 +1,32 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+var _ = require('underscore');
+
 @Component({
-  selector: 'app-list-container',
-  templateUrl: './list-container.component.html',
-  styleUrls: ['./list-container.component.css']
+   selector: 'app-list-container',
+   templateUrl: './list-container.component.html',
+   styleUrls: ['./list-container.component.css']
 })
 export class ListContainerComponent implements OnInit {
-   @Input() private list;
-   @Input() private type;
+   @Input() private list: Array<any>;
+   @Input() private type: string;
 
-  constructor() { }
+   private list_cache: Array<any>;
 
-  ngOnInit() {
-  }
+   constructor() { }
+
+   ngOnInit() {
+      this.list_cache = this.list;
+   }
+
+   searchList(strSearch) {
+      if (strSearch === "") {
+         this.list = this.list_cache;
+      } else {
+         this.list = _.filter(this.list_cache, (list_item) => {
+            return list_item.name.toUpperCase().indexOf(strSearch.toUpperCase()) > -1;
+         });
+      }
+   }
 
 }
