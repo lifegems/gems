f5591f7efe5a10f3e92a062788fe75dadb09aa54
Michalik, Joshua <joshuasmichalik@gmail.com>
2016-10-29 11:12:45 -0400

    Add basic outline for notes location

--- src/app/notible/notible.component.ts
+++ src/app/notible/notible.component.ts
@@ -23,6 +23,10 @@ export class NotibleComponent implements OnInit {
 
    private isNavCollapsed: boolean = true;
 
+   // add new note data
+   private newNoteType: string = 'NOTES';
+   private newNoteVerse: number = 1;
+
    constructor(private bibleService: BibleService, private notesService: NotesService) { }
 
    ngOnInit() {
