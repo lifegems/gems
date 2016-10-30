26b9a3d7feb2634c0bcc0e78af095b1bc6f20543
Michalik, Joshua <joshuasmichalik@gmail.com>
2016-10-29 22:46:00 -0400

    Stylize the various note types

--- src/app/notible/notes/notes.component.ts
+++ src/app/notible/notes/notes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
+import { Note } from './../shared/note';
 import { NotesService } from './notes.service';
 
 let _ = require('underscore');
@@ -13,16 +15,104 @@ export class NotesComponent implements OnInit {
    @Input() private booknumber: number;
    @Input() private chapter: number;
    @Input() private type: string;
+   closeResult: string;
 
-   private notes: Array<any>;
+   private notes: Array<Note>;
+   private selectedNote: Note;
 
-   constructor(private notesService: NotesService) { }
+   constructor(private notesService: NotesService, private modalService: NgbModal) {
+
+   }
 
    ngOnInit() {
-      this.notesService.listNotes(this.booknumber, this.chapter).subscribe((notes) => {
-            this.notes = _.filter(notes, (note) => return note.type === this.type);
-         }, (err) => console.log(err)
-      );
+      this.notes = _.filter(this.listTextNotes(), (note) => {
+         console.log(note.type);
+         return note.type === this.type;
+      });
+      console.log(this.notes);
+      // this.notesService.listNotes(this.booknumber, this.chapter).subscribe((notes) => {
+      //       this.notes = _.filter(notes, (note) => {return note.type === this.type;});
+      //    }, (err) => console.log(err)
+      // );
+   }
+
+
+   open(content, note) {
+      this.selectedNote = note;
+      this.modalService.open(content).result.then((result) => {
+         this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+         this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      });
+   }
+
+   private getDismissReason(reason: any): string {
+      if (reason === ModalDismissReasons.ESC) {
+         return 'by pressing ESC';
+      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+         return 'by clicking on a backdrop';
+      } else {
+         return  `with: ${reason}`;
+      }
+   }
+
+   listTextNotes() {
+      return [
+         {
+            book: 2,
+            chapter: 3,
+            verse: 30,
+            type: 'NOTES',
+            title: 'Words getting explained',
+            description: 'This is an example of how something could get explained.'
+         },
+         {
+            book: 2,
+            chapter: 3,
+            verse: 33,
+            type: 'NOTES',
+            title: 'Word Definition',
+            description: 'Clarify a word simply by showing its definition.'
+         },
+         {
+            book: 19,
+            chapter: 6,
+            verse: 10,
+            type: 'MEDIA',
+            title: "Jehovah's Celestial Chariot",
+            description: "The organization has always been on the move and Jehovah has wanted us to know it!",
+            url: 'https://s-media-cache-ak0.pinimg.com/originals/db/91/19/db91192bf6ace70f3eaa704e9d5d6b4b.jpg'
+         },
+         {
+            book: 19,
+            chapter: 6,
+            verse: 23,
+            type: 'MEDIA',
+            title: 'Four Horsemen of Revelation',
+            description: 'The ride of the four horsemen pictures the turning of events during the last days.',
+            url: 'https://assetsnffrgf-a.akamaihd.net/assets/m/402014047/univ/art/402014047_univ_cnt_2_xl.jpg'
+         },
+         {
+            book: 6,
+            chapter: 17,
+            verse: 1,
+            type: 'XREFS',
+            title: 'Deuteronomy 3:33',
+            description: "This scripture has some text in it. As you read it, you'll realize why",
+            xrefs: "5.3.33",
+            xtype: "General"
+         },
+         {
+            book: 6,
+            chapter: 17,
+            verse: 20,
+            type: 'XREFS',
+            title: 'Judges 8:12',
+            description: "Add some text here and you will have yourselve a scripture.",
+            xrefs: "6.8.12",
+            xtype: "Fulfilled Prophecy"
+         }
+      ];
    }
 
 }
